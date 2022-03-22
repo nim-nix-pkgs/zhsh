bd97926d3005d56a19a686fee2fa951e157af619
hugosenari <hugosenari@gmail.com>
2022-03-22 00:39:27 -0300

    chore: re-index ../nimpkgs/z/zhsh

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-zhsh-v0_2_0.flake = false;
   inputs.src-zhsh-v0_2_0.owner = "PMunch";
-  inputs.src-zhsh-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-zhsh-v0_2_0.ref   = "v0_2_0";
   inputs.src-zhsh-v0_2_0.repo  = "zhangshasha";
   inputs.src-zhsh-v0_2_0.type  = "github";
   
